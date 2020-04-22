d6dbe78db5079f8409fe78a5b148620d8f07913e
Reza Moallemi <rm.allmails@gmail.com>
2020-04-22 11:34:20 +0200

    Implement format method for simple persian date format

--- persian-date/src/main/java/me/moallemi/persiandate/SimplePersianDateFormat.java
+++ persian-date/src/main/java/me/moallemi/persiandate/SimplePersianDateFormat.java
@@ -30,12 +30,83 @@ public class SimplePersianDateFormat {
         this.locale = locale;
     }
 
-    // TODO implement format and date Format
+    /**
+     * y  Year (e.g. 99 or 1399). Use either yy or yyyy.
+     * M  Month in year. Number of M's determine length of format (e.g. MM, MMM or MMMMM)
+     * d  Day in month. Number of d's determine length of format (e.g. d or dd)
+     *
+     * @param date
+     * @return
+     */
     public String format(PersianDate date) {
-        return date.toString();
+        char[] chars = pattern.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            switch (chars[i]) {
+                case 'y':
+                    int yearOccurrence = countOccurrence(chars, chars[i], i);
+                    if (yearOccurrence != 2) {
+                        yearOccurrence = 4;
+                    }
+                    int year = (int) (date.getYear() % Math.pow(10, yearOccurrence));
+                    stringBuilder.append(padLeftZeros(year, yearOccurrence));
+
+                    i += yearOccurrence - 1;
+                    break;
+                case 'M':
+                    int monthOccurrence = countOccurrence(chars, chars[i], i);
+                    if (monthOccurrence > 2) {
+                        stringBuilder.append(date.getMonth());
+                    } else {
+                        int month = (int) (date.getMonthValue() % Math.pow(10, Math.max(monthOccurrence, String.valueOf(date.getMonth()).length())));
+                        stringBuilder.append(padLeftZeros(month, monthOccurrence));
+                    }
+
+                    i += monthOccurrence - 1;
+                    break;
+                case 'd':
+                    int dayOccurrence = countOccurrence(chars, chars[i], i);
+                    int day = (int) (date.getDayOfMonth() % Math.pow(10, Math.max(dayOccurrence, String.valueOf(date.getDayOfMonth()).length())));
+                    stringBuilder.append(padLeftZeros(day, dayOccurrence));
+
+                    i += dayOccurrence - 1;
+                    break;
+                case '-':
+                case '/':
+                case ' ':
+                    stringBuilder.append(chars[i]);
+                    break;
+                default:
+                    throw new PersianDateException("Invalid character in pattern: " + chars[i]);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private int countOccurrence(char[] chars, char character, int index) {
+        int occurrence = 0;
+        while (index < chars.length && chars[index] == character) {
+            occurrence++;
+            index++;
+        }
+        return occurrence;
     }
 
     public PersianDate parse(String source) throws PersianDateException {
         throw new PersianDateException("Not Implemented");
+    }
+
+    private String padLeftZeros(int input, int length) {
+        String inputString = Integer.toString(input);
+        if (inputString.length() >= length) {
+            return inputString;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < length - inputString.length()) {
+            sb.append('0');
+        }
+        sb.append(inputString);
+
+        return sb.toString();
     }
 }
---
+++ persian-date/src/test/java/me/moallemi/persiandate/SimplePersianDateFormatTest.java
@@ -0,0 +1,366 @@
+package me.moallemi.persiandate;
+
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimplePersianDateFormatTest {
+
+    //region Default pattern
+
+    @Test
+    public void testFormat1() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(9, 2, 3);
+        assertEquals("0009/02/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat2() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(9, 12, 3);
+        assertEquals("0009/12/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat3() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(99, 2, 3);
+        assertEquals("0099/02/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat4() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(99, 12, 3);
+        assertEquals("0099/12/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat5() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(399, 2, 3);
+        assertEquals("0399/02/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat6() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(399, 12, 3);
+        assertEquals("0399/12/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat7() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(1399, 2, 3);
+        assertEquals("1399/02/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat8() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(1399, 12, 3);
+        assertEquals("1399/12/03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat9() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(9, 2, 23);
+        assertEquals("0009/02/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat10() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(9, 12, 23);
+        assertEquals("0009/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat11() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(399, 2, 23);
+        assertEquals("0399/02/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat12() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(399, 12, 23);
+        assertEquals("0399/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat13() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(1399, 2, 23);
+        assertEquals("1399/02/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat14() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(99, 12, 23);
+        assertEquals("0099/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat15() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(99, 2, 23);
+        assertEquals("0099/02/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat16() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat();
+        PersianDate persianDate = PersianDate.of(1399, 12, 23);
+        assertEquals("1399/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    //endregion Default pattern
+
+    //region  yy MM dd pattern
+
+    @Test
+    public void testFormat17() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(9, 2, 3);
+        assertEquals("09 02 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat18() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(9, 12, 3);
+        assertEquals("09 12 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat19() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(99, 2, 3);
+        assertEquals("99 02 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat20() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(99, 12, 3);
+        assertEquals("99 12 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat21() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(399, 2, 3);
+        assertEquals("99 02 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat22() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(399, 12, 3);
+        assertEquals("99 12 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat23() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(1399, 2, 3);
+        assertEquals("99 02 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat24() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(1399, 12, 3);
+        assertEquals("99 12 03", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat25() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(9, 2, 23);
+        assertEquals("09 02 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat26() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(9, 12, 23);
+        assertEquals("09 12 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat27() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(399, 2, 23);
+        assertEquals("99 02 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat28() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(399, 12, 23);
+        assertEquals("99 12 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat29() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(1399, 2, 23);
+        assertEquals("99 02 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat30() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(99, 12, 23);
+        assertEquals("99 12 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat31() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(99, 2, 23);
+        assertEquals("99 02 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat32() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yy MM dd");
+        PersianDate persianDate = PersianDate.of(1399, 12, 23);
+        assertEquals("99 12 23", simplePersianDateFormat.format(persianDate));
+    }
+
+    //endregion Default pattern
+
+    //region yyyy/M/d
+
+    @Test
+    public void testFormat33() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(9, 2, 3);
+        assertEquals("0009/2/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat34() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(9, 12, 3);
+        assertEquals("0009/12/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat35() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(99, 2, 3);
+        assertEquals("0099/2/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat36() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(99, 12, 3);
+        assertEquals("0099/12/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat37() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(399, 2, 3);
+        assertEquals("0399/2/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat38() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(399, 12, 3);
+        assertEquals("0399/12/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat39() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(1399, 2, 3);
+        assertEquals("1399/2/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat40() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(1399, 12, 3);
+        assertEquals("1399/12/3", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat41() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(9, 2, 23);
+        assertEquals("0009/2/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat42() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(9, 12, 23);
+        assertEquals("0009/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat43() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(399, 2, 23);
+        assertEquals("0399/2/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat44() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(399, 12, 23);
+        assertEquals("0399/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat45() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(1399, 2, 23);
+        assertEquals("1399/2/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat46() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(99, 12, 23);
+        assertEquals("0099/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat47() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(99, 2, 23);
+        assertEquals("0099/2/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    @Test
+    public void testFormat48() {
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/M/d");
+        PersianDate persianDate = PersianDate.of(1399, 12, 23);
+        assertEquals("1399/12/23", simplePersianDateFormat.format(persianDate));
+    }
+
+    //endregion Default pattern
+
+    @Test
+    public void testFormat49() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MMM/ddd");
+        SimplePersianDateFormat simplePersianDateFormat = new SimplePersianDateFormat("yyyy/MMM/ddd");
+        PersianDate persianDate = PersianDate.of(1399, 12, 23);
+        assertEquals("1399/ESFAND/023", simplePersianDateFormat.format(persianDate));
+    }
+}
